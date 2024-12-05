f6a5f34c40e7adeef6cea634ddf8910e5c97f8db
Saskiasn <h24sasni@du.se>
2024-12-05 20:10:53 +0100

    del2

--- Labb02OP/Program.cs
+++ Labb02OP/Program.cs
@@ -9,29 +9,81 @@ class Program
 
         double windSpeed;
 
-        //Vi skriver ut ett meddelande till användare genom metoden .Write av classen Console
-        Console.Write("Ange utetemperatur i Celsius: ");
 
-        //Läser in användarens med hjälp av svar och lagrar den som en string i identifierare temperatureString
-        string temperatureString = Console.ReadLine();
+        while (true)
+        {
 
-        //Konverterar temperatureString till en double som lagras i identifierare temperature
-        temperature = Convert.ToDouble(temperatureString);
+        Console.WriteLine("-------------------------------------------------");
+
+            //Skriv ut meddelande till användare för val
+        Console.WriteLine("\nHej och vällkommen du kommer nu få två val välj med siffran 1 eller 2\n");
+        Console.WriteLine("1. Beräkna windchill-faktor\n");
+        Console.WriteLine("2. Avsluta programmet\n");
+
+        //Läser in användares knappval
+        char inputChar = Console.ReadKey().KeyChar;
+
+        switch (inputChar)
+        {
+
+            case '1':
+
+                //Vi skriver ut ett meddelande till användare genom metoden .Write av classen Console
+                Console.Write("\n\nAnge utetemperatur i Celsius: ");
+
+                //Läser in användarens med hjälp av svar och lagrar den som en string i identifierare temperatureString
+                string temperatureString = Console.ReadLine();
+
+                //Konverterar temperatureString till en double som lagras i identifierare temperature
+                temperature = Convert.ToDouble(temperatureString);
 
 
-        //Samma princip som rad 13-19
-        Console.Write("Ange vindhastigheten i km/h: ");
+                //Samma princip som rad 13-19
+                Console.Write("Ange vindhastigheten i km/h: ");
 
-        string windSpeedString = Console.ReadLine();
-        windSpeed = Convert.ToDouble(windSpeedString);
-
-
-        //Skapar en ny variabel av data typen double som innehåller en uträkning. Använder mig av metoden .Pow för att räkna ut uphöjt till 0.16.
-        double windChillWCT = 13.12 + 0.6215 * temperature - 11.37 * Math.Pow(windSpeed, 0.16) + 0.3965 * temperature * Math.Pow (windSpeed, 0.16);
+                string windSpeedString = Console.ReadLine();
+                windSpeed = Convert.ToDouble(windSpeedString);
 
 
-        //Skriver ut resultatet till användare
-        Console.WriteLine($"Windchill-faktorn vid en temperatur på {temperature}°C och en vindhastighet på {windSpeed} km/h är: {windChillWCT}");
+                //Skapar en ny variabel av data typen double som innehåller en uträkning. Använder mig av metoden .Pow för att räkna ut uphöjt till 0.16.
+                double windChillWCT = 13.12 + 0.6215 * temperature - 11.37 * Math.Pow(windSpeed, 0.16) + 0.3965 * temperature * Math.Pow(windSpeed, 0.16);
+
+
+                //Skriver ut resultatet till användare
+                Console.WriteLine($"\nWindchill-faktorn vid en temperatur på {temperature}°C och en vindhastighet på {windSpeed} km/h är: {windChillWCT}\n");
+                break;
+
+            case '2':
+
+                Console.WriteLine("\n\nProgrammet avslutas.");
+                Environment.Exit(0);
+                break;
+
+
+            // Trycker användaren på något annat än 1-2 får man felmeddelande
+            default:
+                Console.WriteLine("Ogiltigt val.\n");
+                Console.WriteLine("Programmet avslutas.");
+                Environment.Exit(0);
+                break;
+
+
+        }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
 
